a11af672f3247e161d2b4b554c1e6bcbdd2a6e76
siaozi <siao-zi@hotmail.com>
2019-03-05 14:33:58 +0800

    wrong path modify to init.el

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; in x-window, load more setting
 (if (equal (window-system) 'x)
-    (org-babel-load-file "~/.emcas.d/init_gui.org")
+    (org-babel-load-file "~/.emacs.d/init_gui.org")
     )
 
 ;; load basic setting
