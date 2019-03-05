f50c87c324430e690291936e2b2a6308ae324e7d
siaozi <siao-zi@hotmail.com>
2019-03-05 14:27:31 +0800

    change order of loading gui & cmd

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
-;; load basic setting
-(org-babel-load-file "init_cmd.org")
 ;; in x-window, load more setting
 (if (equal (window-system) 'x)
     (org-babel-load-file "init_gui.org")
     )
+
+;; load basic setting
+(org-babel-load-file "init_cmd.org")
