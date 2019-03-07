19f1666ea9ecb100842192c83dc069f95c040834
siaozi <siao-zi@hotmail.com>
2019-03-07 15:24:01 +0800

    add cmd of create font dir; add auto package update to gui

--- emacs_install.sh
+++ emacs_install.sh
@@ -4,6 +4,7 @@ sudo gem install taskjuggler
 sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
 
+mkdir ~/.local/share/fonts/
 cp ~/.emacs.d/scame/fontPack/* ~/.local/share/fonts/
 fc-cache -f -v
 cp ~/.emacs.d/scame/init.el ~/.emacs.d/
